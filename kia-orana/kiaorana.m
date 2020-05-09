9f88f146ad4cf197735a7a7c036fde32e3dab85c
tumunu <project@tumunu.com>
2020-05-09 22:31:26 +1200

    let's not do this even again!

---
+++ kia-orana/kiaorana.m
@@ -0,0 +1,23 @@
+#import <Foundation/Foundation.h>
+
+@interface KiaOrana : NSObject
+- (void) kiaOrana;
+@end
+
+@implementation KiaOrana
+- (void) kiaOrana {
+    NSLog(@"hello world!");
+}
+@end
+
+int main(void) {
+     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+     
+     KiaOrana *ko = [[KiaOrana alloc] init];
+     [ko kiaOrana];
+     [ko release];
+     
+     [pool drain];
+    
+    return 0;
+}
